79ef7df014a3d5163e7e5d31ce12274615189f23
tridionation <Chris.Mills@tridionation.com>
2018-08-21 13:51:07 +0100

    Module aquisition

--- Scripts/HelloWorld.ps1
+++ Scripts/HelloWorld.ps1
@@ -1,7 +1,20 @@
-Import-Module Visio
+$Modules = @("Visio")
+
+foreach ($Module in $Modules) {
+    if (-not (Get-Module -ListAvailable -Name $Module)) { 
+        if (-not(Get-Module -ListAvailable -Name PowerShellGet)) { 
+            Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force 
+        }
+        Install-Module $Module  -Force 
+    }
+    Import-Module $Module
+}
+
 New-VisioApplication
 New-VisioDocument
-$basic_u = Open-VisioDocument basic_u.vss
-$master = Get-VisioMaster "Rectangle" $basic_u
-$shape = New-VisioShape $master 3,3
-Set-VisioShapeText "Hello World"
+$basic_u = Open-VisioDocument -Filename "basic_u.vss"
+$master = Get-VisioMaster -Name "Rectangle" -Document $basic_u
+
+$shape = New-VisioShape -Masters $master -Points 4,5
+
+Set-VisioText -Text "Hello World" -Shape $shape
